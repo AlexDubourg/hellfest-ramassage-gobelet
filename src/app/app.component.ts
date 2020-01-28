f93239362126eb36fe3d9941b0fc3715798f3281
AlexDubourg <alexandre.dubourg@gmail.com>
2020-01-28 22:11:29 +0100

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { environment } from '../environments/environment';
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
+  mode: any;
   name = 'Angular ';
+
+  constructor() {
+    // import only environment wherever you wanna use.
+    this.mode = environment.mode; 
+  }
 }
